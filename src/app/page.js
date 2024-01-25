441aa61cedc46544a56b32735e38cd2bd1e4f49d
Rick Vermeil <rickv85@gmail.com>
2024-01-25 12:18:17 -0700

    Initial directory and file set up

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+
 import styles from './page.module.css'
 
-export default function Home() {
+export default function Welcome() {
+  // Welcome page here
+  // Enter code, verify on BE, send to app context
   return (
     <main className={styles.main}>
     </main>
