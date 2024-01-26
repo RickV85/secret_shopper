6134b22fb0b91cdd05db17bd0ac12ce5d7165a28
Rick Vermeil <rickv85@gmail.com>
2024-01-26 06:07:36 -0700

    Set up test submit

--- src/app/secret_shopper/page.js
+++ src/app/secret_shopper/page.js
@@ -14,16 +14,37 @@ export default function SecretShopper() {
     });
   };
 
+  const handleSubmit = async () => {
+    // API call to post to api/send_email
+    try {
+      const res = await fetch("/api/send_email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(responses),
+      });
+      if (res.ok) {
+        console.log(res);
+        return res;
+      } else {
+        throw new Error(res);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <main>
-      <form onSubmit={"handleSubmit"}>
+      <form onSubmit={handleSubmit}>
         {/* Create reuseable components that render Yes/No, multiple choice questions, and text inputs
-        then map over an array of questions/available responses with type to inidicate which component to use */}
+        then map over an array of questions/available responses with type to indicate which component to use */}
         {/* legend and fieldset for all questions */}
         <legend>Is this working?</legend>
         <fieldset>
           <input
-            id="yes"
+            id="q1-yes"
             name="q1"
             value={"Yes"}
             type="radio"
@@ -32,7 +53,7 @@ export default function SecretShopper() {
           />
           <label htmlFor="yes">Yes</label>
           <input
-            id="no"
+            id="q1-no"
             name="q1"
             value={"No"}
             type="radio"
@@ -41,6 +62,7 @@ export default function SecretShopper() {
           />
           <label htmlFor="no">No</label>
         </fieldset>
+        <button role="submit">Submit</button>
       </form>
     </main>
   );
