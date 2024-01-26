7cca76b255c0a58df6be40fc6bac025beb523dde
Rick Vermeil <rickv85@gmail.com>
2024-01-26 06:43:29 -0700

    Add successful email send test func

--- src/app/api/send_email/route.js
+++ src/app/api/send_email/route.js
@@ -1,12 +1,35 @@
 import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
 export async function POST(req) {
   try {
     const emailContent = await req.json();
     console.log(emailContent);
 
+    const resend = new Resend(process.env.RESEND_TEST_API_KEY);
+
+    await resend.batch.send([
+      {
+        from: "Rick <rick@rickvermeil.com>",
+        to: ["rickv85@gmail.com"],
+        subject: "hello world",
+        html: `<h1>it works!</h1>
+        <p>${emailContent.q1}</p>
+        `,
+      },
+      // {
+      //   from: "Acme <onboarding@resend.dev>",
+      //   to: ["bar@outlook.com"],
+      //   subject: "world hello",
+      //   html: "<p>it works!</p>",
+      // },
+    ]);
+
     return NextResponse.json({ message: "Success", status: 200 });
   } catch {
-    return NextResponse.json({ message: "Error - message failed", status: 400 });
+    return NextResponse.json({
+      message: "Error - message failed",
+      status: 400,
+    });
   }
 }
