9cb74e0c2b6aa5176b1edfaf064493f715c85706
Rick Vermeil <rickv85@gmail.com>
2024-01-30 13:05:21 -0700

    Add error handling and state reset to CodeInput, complete Welcome page

--- src/app/Components/CodeInput/CodeInput.jsx
+++ src/app/Components/CodeInput/CodeInput.jsx
@@ -7,10 +7,21 @@ import Link from "next/link";
 
 export default function CodeInput() {
   const [userCode, setUserCode] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
   const router = useRouter();
 
   const handleChange = (e) => {
     setUserCode(e.target.value);
+  };
+
+  const failedAuth = () => {
+    setErrorMsg(
+      "Please check the code and try again"
+    );
+    setTimeout(() => {
+      setErrorMsg("");
+      setUserCode("");
+    }, 2000);
   };
 
   const handleSubmit = async () => {
@@ -21,11 +32,13 @@ export default function CodeInput() {
         router.push("/form");
         // Save code to session storage or context
       } else {
-        alert("FAILED AUTH");
+        failedAuth();
       }
     } catch (error) {
       console.error(error);
-      // handle error for user
+      alert(
+        "An error occurred, please refresh the page and try to input your code again."
+      );
     }
   };
 
@@ -33,9 +46,9 @@ export default function CodeInput() {
     <section className={styles["code-input-section"]}>
       <h1 className={styles["welcome-headline"]}>WELCOME, SECRET SHOPPER!</h1>
       <div className={styles["input-div"]}>
-        <label htmlFor="codeInput" className={styles["input-label"]}>
+        {!errorMsg ? <label htmlFor="codeInput" >
           Please enter your code below to begin the survey
-        </label>
+        </label> : <p className={styles.error}>{errorMsg}</p>}
         <input
           id="codeInput"
           className={styles["code-input"]}
