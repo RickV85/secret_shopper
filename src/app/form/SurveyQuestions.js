452acf8b9a78c0afe4412caafb94c78ce5a94d68
Rick Vermeil <rickv85@gmail.com>
2024-02-12 07:48:16 -0700

    Remove question from SurveyQuestions

--- src/app/form/SurveyQuestions.js
+++ src/app/form/SurveyQuestions.js
@@ -1,23 +1,18 @@
 export const surveyQuestions = [
-  // q1
   [
     "multi",
     true,
     "Was the restaurant’s outside appearance attractive? Nice curb appeal?",
     ["Strongly Agree", "Agree", "Disagree", "Strongly Disagree", "Other"],
   ],
-  // q2
   ["text", true, "What was your server's name?"],
-  // q3
   [
     "multi",
     true,
     "Was outside appearance clean-clean windows, porch, doors, etc.?",
     ["Yes", "No"],
   ],
-  // q4
   ["multi", true, "Was the restaurant clean inside?", ["Yes", "No"]],
-  // q5
   [
     "multi",
     true,
@@ -30,71 +25,49 @@ export const surveyQuestions = [
       "1 - Not at all clean or organized",
     ],
   ],
-  // q6
   [
     "multi",
     true,
     "Were the bathrooms well stocked and cleaned?",
     ["Yes", "No"],
   ],
-  // q7
   [
     "multi",
     true,
     "Were you promptly greeted by the host/hostess?",
     ["Yes", "No"],
   ],
-  // q8
-  [
-    "multi",
-    false,
-    "If not immediately greeted, was the host/hostess:",
-    [
-      "on the phone",
-      "talking to staff",
-      "on his or her cell phone",
-      "helping another customer",
-      "no one present",
-    ],
-  ],
-  // q9
   [
     "multi",
     false,
     "If the host/hostess was occupied, did they say that they would be right with you?",
     ["Yes", "No"],
   ],
-  // q10
   [
     "multi",
     true,
     "Was the server knowledgeable about the menu?",
     ["Yes", "No"],
   ],
-  // q11
   [
     "multi",
     true,
     "Did the server suggest a beverage or appetizer?",
     ["Yes", "No"],
   ],
-  // q12
   [
     "multi",
     true,
     "Was the server's appearance appropriate to the nature of the restaurant?",
     ["Yes", "No"],
   ],
-  // q13
   ["multi", true, "Did the food arrive in a timely fashion?", ["Yes", "No"]],
-  // q14
   [
     "multi",
     true,
     "Did the server check back with you during the course of your meal?",
     ["Yes", "No"],
   ],
-  // q15
   [
     "multi",
     true,
@@ -107,28 +80,24 @@ export const surveyQuestions = [
       "Strongly Disagree",
     ],
   ],
-  // q16
   [
     "multi",
     true,
     "What was your overall experience with the customer service?",
     ["5 - Excellent ", "4", "3", "2", "1 - Poor"],
   ],
-  // q17
   [
     "multi",
     true,
     "How would you rate the overall presentation and taste of the food?",
     ["5 - Excellent ", "4", "3", "2", "1 - Poor"],
   ],
-  // q18
   [
     "multi",
     true,
     "How would you rate the quality of the food you received?",
     ["5 - Excellent ", "4", "3", "2", "1 - Poor"],
   ],
-  // q19
   [
     "multi",
     true,
