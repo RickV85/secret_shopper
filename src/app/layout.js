f8310e03cd7a75afb1c3eff96c7bcc0659eda6c0
Rick Vermeil <rickv85@gmail.com>
2024-02-15 07:05:53 -0700

    Add Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={montserrat.variable}>
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
