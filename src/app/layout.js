9e62f8709816135bf357f53eeb190a737176f6d4
Rick Vermeil <rickv85@gmail.com>
2024-02-15 07:13:21 -0700

    Add metadata and OG

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,41 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-montserrat" });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export const metadata = {
-  title: "Buttermilk Kitchen - Secret Shopper",
-  description: "Secret Shopper form for Buttermilk Kitchen Restaurant",
+  title: "Secret Shopper - Buttermilk Kitchen",
+  description: "Secret Shopper survey app for Buttermilk Kitchen Restaurant",
+  robots: {
+    index: false,
+    follow: false,
+  },
+  openGraph: {
+    type: "website",
+    url: "https://buttermilk-shopper.vercel.app",
+    title: "Secret Shopper - Buttermilk Kitchen",
+    description:
+      "Secret Shopper survey app for Buttermilk Kitchen Restaurant",
+    siteName: "Secret Shopper - Buttermilk Kitchen",
+    images: [
+      {
+        url: "/BK_Logo_400.jpg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={montserrat.variable}>
-      <body>{children}</body>
-      <Analytics />
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
